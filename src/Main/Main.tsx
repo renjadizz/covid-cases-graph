3bbb6c18acf08492684faa1d2077be6012531aba
Murat <renja.dizz@gmail.com>
2023-01-16 17:35:04 +0600

    add countryList state

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,3 +1,12 @@
+import {useState} from "react"
+
+export type CountryListType = {
+    id: number
+    country: string
+    data: []
+}
+
 export const Main = () => {
+    const [countryList, setCountryList] = useState<CountryListType[]>([{id: 1, country: "", data: []}])
 
 }

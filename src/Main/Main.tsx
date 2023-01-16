3d8098bece45c3c97fdf16f3a7c092cfbec74e20
Murat <renja.dizz@gmail.com>
2023-01-16 18:44:38 +0600

    add errors alerts

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -5,6 +5,7 @@ import {CovidApi} from "../API/API"
 import {AddSearch} from "../AddSearch/AddSearch"
 import {Graph} from "../Graph/Graph"
 import styles from "./main.module.sass"
+import {Alert, Space} from "antd"
 
 export type CountryListType = {
     id: number
@@ -14,6 +15,7 @@ export type CountryListType = {
 
 export const Main = () => {
     const [countryList, setCountryList] = useState<CountryListType[]>([{id: 1, country: "", data: []}])
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
     const callBackFind = (id: number, value: string) => {
         let newValues = [...countryList]
         if (newValues.length !== 0) {
@@ -31,16 +33,28 @@ export const Main = () => {
         }
     }
     const callBackRemoveSearchField = (id: number) => {
-        let newValues = [...countryList]
-        const countryIndex = newValues.map(e => e.id).indexOf(id)
-        newValues.splice(countryIndex, 1)
-        setCountryList(newValues)
+        if (countryList.length !== 1) {
+            let newValues = [...countryList]
+            const countryIndex = newValues.map(e => e.id).indexOf(id)
+            newValues.splice(countryIndex, 1)
+            setCountryList(newValues)
+        } else {
+            setCountryList([{id: 1, country: "", data: []}])
+            setErrorMessage("minimum size is 1")
+        }
     }
     const callBackAddSearchField = () => {
-        let lastId = countryList.slice(-1)
-        let newId = Number(lastId[0].id) + 1
-        const newValues: CountryListType[] = [...countryList, {id: newId, country: "", data: []}]
-        setCountryList(newValues)
+        if (countryList.length < 5) {
+            let lastId = countryList.slice(-1)
+            let newId = Number(lastId[0].id) + 1
+            const newValues: CountryListType[] = [...countryList, {id: newId, country: "", data: []}]
+            setCountryList(newValues)
+        } else {
+            setErrorMessage("maximum size is 5")
+        }
+    }
+    const onCloseErrorMessage = () => {
+        setErrorMessage(null)
     }
 
     return (
@@ -54,6 +68,13 @@ export const Main = () => {
                                                                       onRemove={callBackRemoveSearchField}/>
                     )}
                 </div>
+                {errorMessage && (<Space align="center" direction="vertical" style={{width: '30%'}}><Alert
+                    message={errorMessage}
+                    showIcon
+                    type="error"
+                    closable
+                    onClose={onCloseErrorMessage}
+                /></Space>)}
                 <div className={styles.btnSearch}><AddSearch onButtonClick={callBackAddSearchField}/></div>
             </div>
             <div className={styles.footer}><Graph countryList={countryList}/></div>

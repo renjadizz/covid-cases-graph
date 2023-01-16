c67c2b25bfb121e086eeb219c39f3c290ec21d59
Murat <renja.dizz@gmail.com>
2023-01-16 17:54:58 +0600

    fetch data to countryList state

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {useState} from "react"
 import { Search } from "../Search/Search"
+import {CovidApi} from "../API/API";
 
 export type CountryListType = {
     id: number
@@ -10,12 +11,37 @@ export type CountryListType = {
 
 export const Main = () => {
     const [countryList, setCountryList] = useState<CountryListType[]>([{id: 1, country: "", data: []}])
+    const callBackFind = (id: number, value: string) => {
+        let newValues = [...countryList]
+        if (newValues.length !== 0) {
+            newValues.forEach((el, i) => {
+                    if (el.id === id) {
+                        newValues[i].country = value
+                        CovidApi.getCountryData(value).then(data => {
+                            newValues[i].data = data.response
+                        }).then(() => {
+                            setCountryList(newValues)
+                        })
+                    }
+                }
+            )
+        }
+    }
+    const callBackRemoveSearchField = (id: number) => {
+            let newValues = [...countryList]
+            const countryIndex = newValues.map(e => e.id).indexOf(id)
+            newValues.splice(countryIndex, 1)
+            setCountryList(newValues)
+    }
+
     return (
         <div>
             <div>
                 {countryList.map((el: CountryListType) => <Search key={el.id}
                                                                   id={el.id}
                                                                   country={el.country}
+                                                                  onSubmit={callBackFind}
+                                                                  onRemove={callBackRemoveSearchField}
                                                                  />
                 )}
             </div>
--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,11 +1,34 @@
 import React from "react"
+import countryData from '../data/all_countries.json'
+import {Select} from 'antd'
 
-type CountryType = {
+type SearchPropsType = {
     id: number
     country: string
+    onSubmit: (id: number, value: string) => void
+    onRemove: (id: number) => void
 }
-export const Search = (props: CountryType) => {
+export const Search = (props: SearchPropsType) => {
+    const onSelect = (value: string) => {
+        props.onSubmit(props.id, value)
+    }
+    const onClear = () => {
+        props.onRemove(props.id)
+    }
+
     return (
-        <div>{props.id} {props.country}</div>
+        <Select
+            placeholder="Select a country"
+            size = "middle"
+            dropdownMatchSelectWidth = {false}
+            allowClear
+            showSearch
+            options={countryData.map((country) => {
+                return ({label: country.country, value: country.country})
+            })}
+            style={{ width: 200 }}
+            onClear={onClear}
+            onSelect={onSelect}
+        />
     )
 }
